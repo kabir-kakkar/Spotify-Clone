102a9a984b1356fc504f539e54e83a700ef4eeb9
Kabir Kakkar <48717801+kabir-kakkar@users.noreply.github.com>
2021-02-17 12:57:16 +0530

    Created a Login Page, but need to enter Client ID

---
+++ src/spotify.js
@@ -0,0 +1,13 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+const redirectUri = "https://localhost:3000/";
+const clientId = ""; // To fill from spotify developers
+
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+];
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
