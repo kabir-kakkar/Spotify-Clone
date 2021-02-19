f2978d712aa04fa69d71ede091379dcec6a2af5b
Kabir Kakkar <48717801+kabir-kakkar@users.noreply.github.com>
2021-02-19 12:35:43 +0530

    Completed the Authoriization Part and now we are getting Tokens from URL

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
 
-
+  
 function App() {
+
+  const [token, setToken] = useState();
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken (_token);
+    }
+
+    console.log ("token", token);
+  }, []);
+
   return (
     <div className="app">
-      <Login />
+      {token ? <h1>Logged in</h1> : <Login />}
     </div>
   );
 }
--- src/spotify.js
+++ src/spotify.js
@@ -1,13 +1,26 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
-const redirectUri = "https://localhost:3000/";
-const clientId = ""; // To fill from spotify developers
+const redirectUri = "http://localhost:3000/";
+const clientId = "763245d3fb154675936a1cc01a611c39";
 
 const scopes = [
-    "user-read-currently-playing",
-    "user-read-recently-played",
-    "user-read-playback-state",
-    "user-top-read",
-    "user-modify-playback-state",
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
 ];
 
-export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
+export const getTokenFromUrl = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      let parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+      return initial;
+    }, {});
+};
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
